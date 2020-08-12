0c0bbc703a3da2e18ebbcae96707570c18ee9d64
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-08-12 13:54:09 -0400

    setup basic sprites and position

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,28 @@
 function preload ()
 {
+  this.load.image('background', 'assets/example.png')
+  this.load.image('character', 'assets/idle01.png')
+  this.load.image('enemy', 'assets/hit02.png')
 }
 
 function create ()
 {
+
+  this.add.image(0, 300, 'background')
+  this.add.image(300, 620, 'character').setScale(3);
+  this.add.image(600, 610, 'enemy').setScale(3);
 }
 
 function update ()
 {
 }
 
+let player;
+
 const config = {
   type: Phaser.AUTO,
   width: 800,
-  height: 600,
+  height: 800,
   scene: {
     preload: preload,
     create: create,
