0cb6ece632d15dc2da8cafb149a66fb08dd9cfc8
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-08-12 12:00:51 -0400

    more basic file setup

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ const PORT = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, "public")));
 
 //////
+//routes
+///
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+//////
 //listen
 /////
 app.listen(PORT, console.log('listening'));
