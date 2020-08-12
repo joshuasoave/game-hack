48e36f3c1d775fe1643cb66b9c68cb8eaa59e0c9
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-08-12 13:24:08 -0400

    basic app

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,37 @@
-console.log('hi');
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0, x: 0 },
+    },
+  }
+};
+
+const gameSettings = {
+  playerSpeed: 10,
+  isIntroDone: false,
+  isGameDone: false,
+  gameTime: 0
+}
+
+const game = new Phaser.Game(config);
