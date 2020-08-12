effaacf3f77c006bbfda392d601dc676f11537d8
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-08-12 14:21:22 -0400

    player can move

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,6 @@
-function preload ()
-{
-  this.load.image('background', 'assets/example.png')
-  this.load.image('character', 'assets/idle01.png')
-  this.load.image('enemy', 'assets/hit02.png')
-}
-
-function create ()
-{
-
-  this.add.image(0, 300, 'background')
-  this.add.image(300, 620, 'character').setScale(3);
-  this.add.image(600, 610, 'enemy').setScale(3);
-}
-
-function update ()
-{
-}
-
-let player;
-
+/////////
+//config
+////////
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -41,6 +23,63 @@ const gameSettings = {
   isIntroDone: false,
   isGameDone: false,
   gameTime: 0
+}
+
+/////
+//init
+////
+let player;
+let enemy;
+
+
+/////
+///functions
+/////
+function preload ()
+{
+  this.load.image('background', './assets/example.png')
+  this.load.image('character', './assets/idle01.png')
+  this.load.image('enemy', './assets/hit02.png')
+}
+
+function create ()
+{
+
+  this.add.image(0, 300, 'background')
+
+  this.add.image(600, 610, 'enemy').setScale(3);
+
+  ////////
+  //player
+  /////////
+  player = this.physics.add.image(300, 620, 'character').setScale(3);
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+
+  //  Input Events
+  cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update ()
+{
+  if (cursors.left.isDown)
+  {
+    player.setVelocityX(-160);
+  }
+  else if (cursors.right.isDown)
+  {
+    player.setVelocityX(160);
+  }
+  else
+  {
+    player.setVelocityX(0);
+  }
+
+if (cursors.up.isDown && player.body.touching.down)
+  {
+    player.setVelocityY(-330);
+  }
 }
 
 const game = new Phaser.Game(config);
